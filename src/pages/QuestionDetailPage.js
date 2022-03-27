acb83b0ee929e70f5fa8cd9b53d867f81c4f65c9
taihoang <tai47406@donga.edu.vn>
2022-03-27 14:06:03 +0700

    push code for detail question page

--- src/pages/QuestionDetailPage.js
+++ src/pages/QuestionDetailPage.js
@@ -46,7 +46,7 @@ function QuestionDetailPage() {
     }, []);
     console.log(scrollPosition);
 
-    
+
     return (
         <Container
             maxWidth="xl"
@@ -98,7 +98,7 @@ function QuestionDetailPage() {
                         pl: 2.5
                     }}
                 >
-                    Đây là những câu trả lời tốt nhất cho câu hỏi của bạn
+                    Đây là tiêu đề cho câu hỏi của bạn
                 </Typography>
                 <Box
                     sx={{
@@ -109,6 +109,7 @@ function QuestionDetailPage() {
                         alignItems: 'flex-start',
                         boxSizing: 'border-box',
                         p: 2.5,
+                        pt: 1,
                         // boxShadow:' rgba(149, 157, 165, 0.2) 0px 8px 24px',
                         gap: 2
                     }}
@@ -119,7 +120,7 @@ function QuestionDetailPage() {
                             flexDirection: 'row',
                             justifyContent: 'flex-start',
                             alignItems: 'center',
-                            gap: 1
+                            gap: 2
 
                         }}
                     >
@@ -224,7 +225,7 @@ function QuestionDetailPage() {
                         </Box>
                     </Box>
                 </Box>
-                {/* <Box
+                <Box
                     sx={{
                         width: '100%',
                         display: 'flex',
@@ -258,7 +259,7 @@ function QuestionDetailPage() {
                             justifyContent: 'flex-start',
                             alignItems: 'center',
                             // pt: 2,
-                            gap: 1
+                            gap: 2
 
                         }}
                     >
@@ -272,19 +273,32 @@ function QuestionDetailPage() {
                                 display: 'flex',
                                 flexDirection: 'column',
                                 justifyContent: 'space-around',
-                                alignItems: 'flex-start'
+                                alignItems: 'flex-start',
+                                height: '50px!important'
                             }}
                         >
-                            <Typography
+                            <Box
                                 sx={{
-                                    fontWeight: 600,
-                                    m: 0,
-                                    fontSize: 16
+                                    display: 'flex',
+                                    flexDirection:'row',
+                                    justifyContent:'flex-start',
+                                    alignItems: 'flex-start',
+                                    gap: 2
                                 }}
-                                variant="h6"
-                                gutterBottom component="div">
-                            Hoàng Hữu Tài
-                            </Typography>
+                            >
+                                <Typography
+                                    sx={{
+                                        fontWeight: 600,
+                                        m: 0,
+                                        fontSize: 16
+                                    }}
+                                    variant="h6"
+                                    gutterBottom component="div">
+                                        Hoàng Hữu Tài
+                                </Typography>
+                                <Chip sx={{ color: lightGreen['A400'], backgroundColor: green[50] }} label="Best Answer" />
+                            </Box>
+
                             <Typography
                                 sx={{
                                     color: grey[400],
@@ -296,19 +310,7 @@ function QuestionDetailPage() {
                             4 days ago
                             </Typography>
                         </Box>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection:'column',
-                                justifyContent:'flex-start',
-                                alignItems: 'flex-start',
-                                height: '100%'
-                            }}
-                        >
-                            <Stack direction="row" spacing={1}>
-                                <Chip label="Best Answer" />
-                            </Stack>
-                        </Box>
+
 
                     </Box>
                     <Box>
@@ -316,7 +318,8 @@ function QuestionDetailPage() {
                             sx={{
                                 fontWeight: 600,
                                 m: 0,
-                                fontSize: 15
+                                fontSize: 15,
+                                color: grey[600]
                             }}
                             variant="h6"
                             gutterBottom component="div">
@@ -326,7 +329,8 @@ function QuestionDetailPage() {
                             sx={{
                                 fontWeight: 600,
                                 m: 0,
-                                fontSize: 15
+                                fontSize: 15,
+                                color: grey[600]
                             }}
                             variant="h6"
                             gutterBottom component="div">
@@ -348,7 +352,8 @@ function QuestionDetailPage() {
                             sx={{
                                 fontWeight: 600,
                                 m: 0,
-                                fontSize: 15
+                                fontSize: 15,
+                                color: grey[600]
                             }}
                             variant="h6"
                             gutterBottom component="div">
@@ -391,20 +396,6 @@ function QuestionDetailPage() {
                         // boxShadow:' rgba(149, 157, 165, 0.2) 0px 8px 24px'
                     }}
                 >
-                    <Typography
-                        sx={{
-                            // color: grey[400],
-                            m: 0,
-                            fontSize: 16,
-                            fontWeight: 600,
-                            width: '100%',
-                            pb: 1,
-                            borderBottom: '1px solid #d9d8d7'
-                        }}
-                        variant="subtitle1"
-                        gutterBottom component="div">
-                            12 Câu trả lời
-                    </Typography>
                     <Box
                         sx={{
                             display: 'flex',
@@ -412,7 +403,7 @@ function QuestionDetailPage() {
                             justifyContent: 'flex-start',
                             alignItems: 'center',
                             // pt: 2,
-                            gap: 1
+                            gap: 2
 
                         }}
                     >
@@ -450,17 +441,6 @@ function QuestionDetailPage() {
                             4 days ago
                             </Typography>
                         </Box>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection:'column',
-                                justifyContent:'flex-start',
-                                alignItems: 'flex-start',
-                                height: '100%'
-                            }}
-                        >
-                            <Chip label="Best Answer" />
-                        </Box>
 
                     </Box>
                     <Box>
@@ -468,7 +448,8 @@ function QuestionDetailPage() {
                             sx={{
                                 fontWeight: 600,
                                 m: 0,
-                                fontSize: 15
+                                fontSize: 15,
+                                color: grey[600]
                             }}
                             variant="h6"
                             gutterBottom component="div">
@@ -478,7 +459,8 @@ function QuestionDetailPage() {
                             sx={{
                                 fontWeight: 600,
                                 m: 0,
-                                fontSize: 15
+                                fontSize: 15,
+                                color: grey[600]
                             }}
                             variant="h6"
                             gutterBottom component="div">
@@ -500,7 +482,8 @@ function QuestionDetailPage() {
                             sx={{
                                 fontWeight: 600,
                                 m: 0,
-                                fontSize: 15
+                                fontSize: 15,
+                                color: grey[600]
                             }}
                             variant="h6"
                             gutterBottom component="div">
@@ -530,158 +513,6 @@ function QuestionDetailPage() {
                         </Typography>
                     </Box>
                 </Box>
-                <Box
-                    sx={{
-                        width: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-between',
-                        alignItems: 'flex-start',
-                        boxSizing: 'border-box',
-                        gap: 2,
-                        p: 2.5
-                        // boxShadow:' rgba(149, 157, 165, 0.2) 0px 8px 24px'
-                    }}
-                >
-                    <Typography
-                        sx={{
-                            // color: grey[400],
-                            m: 0,
-                            fontSize: 16,
-                            fontWeight: 600,
-                            width: '100%',
-                            pb: 1,
-                            borderBottom: '1px solid #d9d8d7'
-                        }}
-                        variant="subtitle1"
-                        gutterBottom component="div">
-                            12 Câu trả lời
-                    </Typography>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'flex-start',
-                            alignItems: 'center',
-                            // pt: 2,
-                            gap: 1
-
-                        }}
-                    >
-                        <Stack direction="row">
-                            <Avatar
-                                alt="Avatar"
-                                src= {user4} />
-                        </Stack>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'space-around',
-                                alignItems: 'flex-start'
-                            }}
-                        >
-                            <Typography
-                                sx={{
-                                    fontWeight: 600,
-                                    m: 0,
-                                    fontSize: 16
-                                }}
-                                variant="h6"
-                                gutterBottom component="div">
-                            Hoàng Hữu Tài
-                            </Typography>
-                            <Typography
-                                sx={{
-                                    color: grey[400],
-                                    m: 0,
-                                    fontSize: 14
-                                }}
-                                variant="subtitle1"
-                                gutterBottom component="div">
-                            4 days ago
-                            </Typography>
-                        </Box>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection:'column',
-                                justifyContent:'flex-start',
-                                alignItems: 'flex-start',
-                                height: '100%'
-                            }}
-                        >
-                            <Chip label="Best Answer" />
-                        </Box>
-
-                    </Box>
-                    <Box>
-                        <Typography
-                            sx={{
-                                fontWeight: 600,
-                                m: 0,
-                                fontSize: 15
-                            }}
-                            variant="h6"
-                            gutterBottom component="div">
-                            Tôi chưa dùng thư viện này bao giờ, nhưng tôi có thể giúp bạn bằng một số dòng code dưới đây:
-                        </Typography>
-                        <Typography
-                            sx={{
-                                fontWeight: 600,
-                                m: 0,
-                                fontSize: 15
-                            }}
-                            variant="h6"
-                            gutterBottom component="div">
-                            Dưới đây là phần code mẫu cho bạn:
-                        </Typography>
-                    </Box>
-                    <Box
-                        sx={{
-                            width: '100%',
-                            boxSizing: 'border-box',
-                            p: 2.5,
-                            backgroundColor: '#f0f4f5'
-                        }}
-                    >
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repudiandae, optio at non eligendi error dolore quibusdam est minima consequatur, cupiditate explicabo totam assumenda repellat? Itaque hic molestias nobis dicta.
-                    </Box>
-                    <Box>
-                        <Typography
-                            sx={{
-                                fontWeight: 600,
-                                m: 0,
-                                fontSize: 15
-                            }}
-                            variant="h6"
-                            gutterBottom component="div">
-                            Hoặc bạn có thể thử cách khác dưới đây:
-                        </Typography>
-                    </Box>
-                    <Box
-                        sx={{
-                            width: '100%',
-                            boxSizing: 'border-box',
-                            p: 2.5,
-                            backgroundColor: '#f0f4f5'
-                        }}
-                    >
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repudiandae, optio at non eligendi error dolore quibusdam est minima consequatur, cupiditate explicabo totam assumenda repellat? Itaque hic molestias nobis dicta.
-                    </Box>
-                    <Box>
-                        <Typography
-                            sx={{
-                                fontWeight: 600,
-                                m: 0,
-                                fontSize: 15
-                            }}
-                            variant="h6"
-                            gutterBottom component="div">
-                            Check this link: <Link to={'#'}>https://github.com/quyen46919/UDA_Stackoverflow</Link>
-                        </Typography>
-                    </Box>
-                </Box> */}
             </Box>
         </Container>
     );
